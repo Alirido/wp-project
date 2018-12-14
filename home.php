3303e9c7cb60f5290c444237984d9f455c0be8c9
Ali Rido <mohalirido@gmail.com>
2018-12-14 16:59:29 +0700

    Add custom page for this project

---
+++ home.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rapot mahasiswa</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- <link rel="stylesheet" type="text/css" href="style.css" > -->
+<style>
+	body {
+		font-family: Arial, Helvetica, sans-serif;;
+	    margin: 0;
+	    background-color: #8D15E8;
+	}
+
+	table tr td:first-child {
+		text-align: left;
+		width:30%;
+	}
+
+	table tr td:nth-child(2) {
+		text-align: center;
+		width:40%;
+	}
+
+	table tr td:last-child {
+		text-align: right;
+		width:30%;
+	}
+
+	table tr td {
+		/*border: 1px solid red;*/
+	}
+
+	/*--------------- HEADER ---------------*/
+	header {
+		background-color: #8D15E8;
+	}
+
+	table.header {
+		width: 100%;
+		padding: 0;
+		margin: 0;
+		/*border: 1px solid red;*/
+	}
+
+	table.header tr:first-child td {
+		padding: 1em 0.5em;
+		/*border-bottom: 1px solid white;*/
+	}
+
+	/*--------------- TOPNAV ---------------*/
+	table.topnav {
+		background-color: #C19BDE;
+		width: 100%;
+	}
+
+	table.topnav tr td {
+		/*border: 1px solid yellow;	*/
+		padding: 0.6em 0.5em;
+		margin: 0;
+	}
+
+</style>
+
+</head>
+<body>
+
+	<!-- HEADER -->
+	<header>
+
+	    <table class="header">
+	        <tr>
+	            <td id="left-header">LOGO#1</th>
+	            <td id="middle-header">AMERTA</th>
+	            <td id="right-header">LOGO#2</i></a></th>
+	        </tr>
+	    </table>
+
+	</header>
+
+	<table class="topnav">
+		<tr >
+			<td id="left-header">Back to Home</td>
+			<td id="middle-header">Generation<i class="fa fa-caret-down"></i></td>
+			<td id="right-header">LOGO#2</i></a></td>
+	    </tr>
+	</table>
+
+
+	<div class="content">
+
+	</div>
+</body>
+</html>
