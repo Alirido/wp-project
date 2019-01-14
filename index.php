ef4067c76301d39f43327fe6b3a9f617e07f9969
Ali Rido <mohalirido@gmail.com>
2019-01-14 11:14:57 +0700

    Update frontend index.php

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 <html>
 <head>
 	<title>Rapot mahasiswa</title>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+
 	<!-- <link rel="stylesheet" type="text/css" href="style.css" > -->
 <style>
 	body {
@@ -42,18 +45,6 @@
 
 	/*--------------- HEADER ---------------*/
 
-	table.header {
-		width: 100%;
-		padding: 0;
-		margin: 0;
-		/*border: 1px solid red;*/
-	}
-
-	table.header tr:first-child td {
-		/*padding: 1em 0.5em;*/
-		/*border-bottom: 1px solid white;*/
-	}
-
 	/*--------------- TOPNAV ---------------*/
 	table.topnav {
 		background-color: #C19BDE;
@@ -91,8 +82,16 @@
 
 	 }
 
-	table#test td {
+
+	table#test i {
 		/*border: 1px solid red;*/
+		color: white;
+		font-size: 10em;
+	}
+
+	table#test td {
+		/*border: 1px solid yellow;*/
+		text-align: center;
 	}
 
 </style>
@@ -105,9 +104,9 @@
 
 	    <table class="top">
 	        <tr>
-	            <td><img src="assets/logo_archaea.jpg" width="144" height="144"></th>
-	            <td style="">AMERTA</th>
-	            <td>LOGO#2</i></a></th>
+	            <td><img src="assets/logo_archaea.jpg" width="144" height="144"></td>
+	            <td>AMERTA</td>
+	            <td>LOGO#2</td>
 	        </tr>
 	    </table>
 
@@ -120,8 +119,10 @@
 		<li><a href="#about">About</a></li>
 	</ul> -->
 
+	<!-- Coba ambi top menu nya dari yg udah dipake di wordpress-nya -->
+
 	<table class="topnav">
-		<tr >
+		<tr style="color:black">
 			<td>Back to Home</td>
 			<td>Generation<i class="fa fa-caret-down"></i></td>
 			<td>LOGO#2</i></a></td>
@@ -138,12 +139,14 @@
 		<br><br><br><br><br>
 
 		<table id="test" style="width: 100%">
+			
 			<tr>
-				<td style="width: 20%"><img src="assets/username_icon.png" width="216" height="216"></td>
-				<td style="width: 20%"><img src="assets/username_icon.png" width="216" height="216"></td>
-				<td style="width: 20%"><img src="assets/username_icon.png" width="216" height="216"></td>
-				<td style="width: 20%"><img src="assets/username_icon.png" width="216" height="216"></td>
-				<td style="width: 20%"><img src="assets/username_icon.png" width="216" height="216"></td>
+				<td style="width: 20%"><i class="fas fa-user"></i></td>
+				<td style="width: 20%"><i class="fas fa-user"></i></td>
+				<td style="width: 20%"><i class="fas fa-user"></i></td>
+				<td style="width: 20%"><i class="fas fa-user"></i></td>
+				<td style="width: 20%"><i class="fas fa-user"></i></td>
+				<!-- <img src="assets/username_icon.png" width="216" height="216"> -->
 			</tr>
 
 		</table>
