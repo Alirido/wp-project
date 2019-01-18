297197b0d2d988c5ed8ef26ea29e92b9a7093f2b
Ali Rido <mohalirido@gmail.com>
2019-01-18 16:29:43 +0700

    Update login page

--- loginPage.php
+++ loginPage.php
@@ -6,7 +6,7 @@
 
 	<title>Log In</title>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="assets/login.css" >
+	<link rel="stylesheet" type="text/css" href="http://archaea.sith.itb.ac.id/wp-content/themes/zerif-lite/custom/assets/login.css" >
 
 </head>
 <body>
@@ -15,7 +15,7 @@
 
         <h1>LOG IN</h1>
 
-        <form name="regis" method="POST" onsubmit="return validateRegister()" action="backend/_register.php">
+        <form name="regis" method="POST" onsubmit="return validateRegister()" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             
             <table>
                 <tr>
