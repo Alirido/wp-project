4cd1950b827b988029586f6b027f3f8943ece7a7
Ali Rido <mohalirido@gmail.com>
2019-02-04 16:57:22 +0700

    Add radar chart

--- profilePage.php
+++ profilePage.php
@@ -4,7 +4,7 @@ $nim = 0;
 if (isset($_GET['nim'])) {
 	$nim = $_GET['nim'];
 } else {
-	header("location: http://archaea.sith.itb.ac.id/rapot-himpunan");
+	header("location: https://archaea.sith.itb.ac.id/rapot-himpunan");
 }
 
 // QUERY
@@ -18,7 +18,9 @@ $result = $wpdb->get_results( "SELECT * FROM mahasiswa WHERE nim=$nim" );
 
 	<title>Profile</title>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="http://archaea.sith.itb.ac.id/wp-content/themes/zerif-lite/custom/assets/style.css" >
+	<link rel="stylesheet" type="text/css" href="https://archaea.sith.itb.ac.id/wp-content/themes/zerif-lite/custom/assets/style.css">
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 
 </head>
 <body>
@@ -28,9 +30,9 @@ $result = $wpdb->get_results( "SELECT * FROM mahasiswa WHERE nim=$nim" );
 
 	    <table class="top">
 	        <tr>
-	            <td><img width="144" height="144" src="http://archaea.sith.itb.ac.id/wp-content/uploads/2016/09/cropped-cropped-FIX-ARCHAEA-3-1-2.png" class="custom-logo" alt="cropped-cropped-FIX-ARCHAEA-3-1-2.png" itemprop="logo" srcset="http://archaea.sith.itb.ac.id/wp-content/uploads/2016/09/cropped-cropped-FIX-ARCHAEA-3-1-2.png 1004w, http://archaea.sith.itb.ac.id/wp-content/uploads/2016/09/cropped-cropped-FIX-ARCHAEA-3-1-2-150x150.png 150w, http://archaea.sith.itb.ac.id/wp-content/uploads/2016/09/cropped-cropped-FIX-ARCHAEA-3-1-2-300x300.png 300w, http://archaea.sith.itb.ac.id/wp-content/uploads/2016/09/cropped-cropped-FIX-ARCHAEA-3-1-2-768x770.png 768w" sizes="(max-width: 1004px) 100vw, 1004px"></td>
+	            <td><img width="144" height="144" src="https://archaea.sith.itb.ac.id/wp-content/uploads/2016/09/cropped-cropped-FIX-ARCHAEA-3-1-2.png" class="custom-logo" alt="cropped-cropped-FIX-ARCHAEA-3-1-2.png" itemprop="logo" srcset="https://archaea.sith.itb.ac.id/wp-content/uploads/2016/09/cropped-cropped-FIX-ARCHAEA-3-1-2.png 1004w, https://archaea.sith.itb.ac.id/wp-content/uploads/2016/09/cropped-cropped-FIX-ARCHAEA-3-1-2-150x150.png 150w, https://archaea.sith.itb.ac.id/wp-content/uploads/2016/09/cropped-cropped-FIX-ARCHAEA-3-1-2-300x300.png 300w, https://archaea.sith.itb.ac.id/wp-content/uploads/2016/09/cropped-cropped-FIX-ARCHAEA-3-1-2-768x770.png 768w" sizes="(max-width: 1004px) 100vw, 1004px"></td>
 	            <td>AMERTA</td>
-	            <td><img class="details-image" src="http://archaea.sith.itb.ac.id/wp-content/uploads/2018/04/archaeamantap.png" draggable="false" alt="archaea_mantap" width="144" height="144"></td>
+	            <td><img class="details-image" src="https://archaea.sith.itb.ac.id/wp-content/uploads/2018/04/archaeamantap.png" draggable="false" alt="archaea_mantap" width="144" height="144"></td>
 	        </tr>
 	    </table>
 
@@ -43,13 +45,13 @@ $result = $wpdb->get_results( "SELECT * FROM mahasiswa WHERE nim=$nim" );
 			<td><a href="https://archaea.sith.itb.ac.id/">Home</a></td>
 			<td class="dropbtn" onclick="myFunction()">Angkatan <i class="fa fa-caret-down"></i>
 				<div class="dropdown-content" id="myDropdown">
-					<a href="http://archaea.sith.itb.ac.id/rapot-himpunan?angkatan=2015">2015</a>
-					<a href="http://archaea.sith.itb.ac.id/rapot-himpunan?angkatan=2016">2016</a>
-					<a href="http://archaea.sith.itb.ac.id/rapot-himpunan?angkatan=2017">2017</a>
+					<a href="https://archaea.sith.itb.ac.id/rapot-himpunan?angkatan=2015">2015</a>
+					<a href="https://archaea.sith.itb.ac.id/rapot-himpunan?angkatan=2016">2016</a>
+					<a href="https://archaea.sith.itb.ac.id/rapot-himpunan?angkatan=2017">2017</a>
 				</div>
 			</td>
 			<td><a href="https://archaea.sith.itb.ac.id/kontak/">Kontak</a></td>
-			<td><a href="http://archaea.sith.itb.ac.id/login-rapot/">Masuk</a></td>
+			<td><a href="https://archaea.sith.itb.ac.id/login-rapot/">Masuk</a></td>
 	    </tr>
 
 	</table>
@@ -64,7 +66,7 @@ $result = $wpdb->get_results( "SELECT * FROM mahasiswa WHERE nim=$nim" );
 			<div>
 				<?php 
 				if ($print->foto != "NO") {
-					echo '<img class="details-image" src="http://archaea.sith.itb.ac.id/wp-content/uploads/photo-profile/'.$print->angkatan.'/'.$print->nim.'.'.$print->foto.'" alt="'.$print->nim.'" width="400" height="400">';
+					echo '<img class="details-image" src="https://archaea.sith.itb.ac.id/wp-content/uploads/photo-profile/'.$print->angkatan.'/'.$print->nim.'.'.$print->foto.'" alt="'.$print->nim.'" width="400" height="400">';
 				} else {
 					echo '<i class="fas fa-user-circle" style="float: left; color: white; font-size: 15em"></i>';
 				}
@@ -91,10 +93,13 @@ $result = $wpdb->get_results( "SELECT * FROM mahasiswa WHERE nim=$nim" );
 			</div>
 		</div>
 		<div class="right">
-			<div>
-				<img src="assets/radar-chart.png">
+
+			<div style="background-color: white">
+				<canvas id="myChart"></canvas>
 			</div>
+			
 			<br>
+			
 			<div class="presensi">
 				<span>Presensi ...% (14/20)</span><br>
 				<div style="margin-left: 0.4em">
@@ -106,29 +111,9 @@ $result = $wpdb->get_results( "SELECT * FROM mahasiswa WHERE nim=$nim" );
 					- Kemasyarakatan ... % () 
 				</div>
 			</div>
+
 		</div>
 
-		<!-- <table id="test" style="width: 100%">
-			
-			<tr>
-				<td style="width: 75%"><img src="assets/ava_icon.png" width="288" height="288" style="margin-left: 2em; float: left"><br><br><br><span style="font-size: 1.5em; font-weight: bold; padding-left: 0.5em">Nama: .....</span><br>
-				<span style="font-size: 1.5em; font-weight: bold; padding-left: 0.5em">NIM: .....</span><br>
-				<span style="font-size: 1.5em; font-weight: bold; padding-left: 0.5em">Jabatan: .....</span></td>
-				<td style="width: 25%"><img src="assets/radar-chart.png"><br> -->
-					<!-- <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> -->
-					<!-- <br>
-					Presensi ...% (14/20)<br>
-					- kolonisasi ...% (4/4)<br>
-					- ... <br>
-					- ... <br>
-					- ... <br>
-				</td>
-
-
-			</tr>
-
-		</table> -->
-		<!-- <br><br><br><br><br> -->
 		<?php
 		}
 		?>
@@ -136,6 +121,52 @@ $result = $wpdb->get_results( "SELECT * FROM mahasiswa WHERE nim=$nim" );
 	</div>
 
 <script>
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var myRadarChart = new Chart(ctx, {
+    type: 'radar',
+    data: {
+	    labels: ['Hiburan & Internalisasi', 'Forum & Kajian', 'Pengembangan Anggota', 'Kebutuhan Dasar', 'Keprofesian', 'Kemasyarakatan'],
+	    datasets: [{
+	    	label: "Kehadiran",
+	    	backgroundColor: 'rgb(100, 99, 132)',
+	    	// 'rgb(255, 99, 132)',
+	    	borderColor: 'rgb(100, 99, 132)',
+	    	data: [5, 8, 4, 7, 5, 6]
+	        // data: [20, 10, 4, 2]
+	    }]
+	},
+    options: {
+    	scale: {
+    		ticks: {
+    			//beginAtZero: true,
+    			min: 2,
+    			max: 10
+    		}
+    	}
+    }
+});
+
+// var chart = new Chart(ctx, {
+//     // The type of chart we want to create
+//     type: 'line',
+
+//     // The data for our dataset
+//     data: {
+//         labels: ["January", "February", "March", "April", "May", "June", "July"],
+//         datasets: [{
+//             label: "My First dataset",
+//             backgroundColor: 'rgb(255, 99, 132)',
+//             borderColor: 'rgb(255, 99, 132)',
+//             data: [0, 10, 5, 2, 20, 30, 45],
+//         }]
+//     },
+
+//     // Configuration options go here
+//     options: {}
+// });
+
 	/* When the user clicks on the button, 
 	toggle between hiding and showing the dropdown content */
 	function myFunction() {
