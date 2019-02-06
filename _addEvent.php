af094218bdf65823301554e773888e5b1552c187
Ali Rido <mohalirido@gmail.com>
2019-02-06 13:37:32 +0700

    Update constraint to input event name

--- _addEvent.php
+++ _addEvent.php
@@ -11,21 +11,26 @@ if (isset($_POST['acara']) && isset($_POST['divisi']) && isset($_POST['id'])) {
 	$div = $wpdb->escape($_POST['divisi']);
 	$id = $wpdb->escape($_POST['id']);
 
-	if (($wpdb->insert('event_archaea', 
-		array(
-			'id' => $id,
-			'acara' => $acara,
-			'divisi' => $div
-		),
-		array(
-			'%d',
-			'%s',
-			'%s')))) {
-		// Berhasil menambahkan
-		echo 'Acara "'.$acara.'" berhasil ditambahkan.';
+	$check = $wpdb->get_var("SELECT COUNT(id) FROM event_archaea WHERE acara='$acara'");
+	if ($check == 0) {
+		if (($wpdb->insert('event_archaea', 
+			array(
+				'id' => $id,
+				'acara' => $acara,
+				'divisi' => $div
+			),
+			array(
+				'%d',
+				'%s',
+				'%s')))) {
+			// Berhasil menambahkan
+			echo 'Acara "'.$acara.'" berhasil ditambahkan.';
+		} else {
+			// Gagal menambahkan
+			echo 'Acara "'.$acara.'" gagal ditambahkan.';
+		}
 	} else {
-		// Gagal menambahkan
-		echo 'Acara "'.$acara.'" gagal ditambahkan.';
+		echo 'Acara "'.$acara.'" sudah ada.';
 	}
 } else {
 	echo "Nothing post method happen!";
